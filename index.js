63f8366d5e4158136caf1d70b0b94d3cfbd125aa
Mepromabat <132379784+Mepromabat@users.noreply.github.com>
2023-09-21 13:56:48 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+document.querySelector("#add").onclick = function(){
+    if(document.querySelector(".newTask input").value.length == 0){
+        alert("Add task name!");
+    } else {
+        document.querySelector("#tasks").innerHTML +=
+       `<div class="task">
+            <span id="taskname">
+                ${document.querySelector(".newTask input").value}
+           </span>
+           <button class="delete">
+           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!-- Font Awesome Pro 5.15.4 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) --><path d="M32 464a48 48 0 0 0 48 48h288a48 48 0 0 0 48-48V128H32zm272-256a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zM432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16z"/></svg>
+          </button>
+          <button class="done">
+          <svg xmlns="http://www.w3.org/2000/svg" height="2rem" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>
+         </button>
+        </div>
+        `;
+        var currentTask = document.querySelectorAll(".delete");
+        for (var i=0; i<currentTask.length; i++) {
+            currentTask[i].onclick = function(){
+                this.parentNode.remove();
+            }    
+        }
+    }
+}
